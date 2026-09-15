94b49fe264a4cbc82e941119bb4e270b0c6e9f9e
Kanwal Shahzadi <kanwlshakeel345@gmail.com>
2026-09-15 20:47:11 +0500

    Initial commit from Create Next App

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+
+    <p>Hello I'm</p>
+    <h1>Kanwal Shakeel</h1>
+    <h2>MERN Stack </h2>
+
+    </div>
+  )
+}
+
+export default Hero
